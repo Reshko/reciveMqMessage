5be1be3f26ac30ff0164fc48203440673d3fb837
Aleksey Kolpakov <kolpak.al6@yandex.ru>
2022-03-20 15:25:38 +0300

    fix

--- src/main/java/ru/mts/emulator/Listener.java
+++ src/main/java/ru/mts/emulator/Listener.java
@@ -22,24 +22,15 @@ public class Listener {
             text = msg.toString();
         }
 
-
-        System.out.println();
-        System.out.println("========================================");
-
-        System.out.println("Responder received message: " + text);
-        System.out.println("           Redelivery flag: " + msg.getJMSReplyTo());
-        System.out.println("========================================");
-
-
         final String msgID = msg.getJMSMessageID();
         final String corrID = msg.getJMSCorrelationID();
 
-
-        MessageProducer replyDest = session.createProducer(msg.getJMSDestination());
-        TextMessage replyMsg = session.createTextMessage("Replying to " + text);
-        replyMsg.setJMSCorrelationID(corrID);
-        replyMsg.setJMSMessageID(msgID);
-        replyDest.send(replyMsg);
+        Destination destination = session.createQueue(responseQueue);
+        MessageProducer newDest = session.createProducer(destination);
+        TextMessage responseMsg = session.createTextMessage("Replying to " + text);
+        responseMsg.setJMSCorrelationID(corrID);
+        responseMsg.setJMSMessageID(msgID);
+        newDest.send(responseMsg);
 
 
     }
