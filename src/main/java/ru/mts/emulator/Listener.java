7c25cc4326224651ce606d57ddd7360e66d29ebb
Aleksey Kolpakov <kolpak.al6@yandex.ru>
2022-03-20 15:11:09 +0300

    fix

--- src/main/java/ru/mts/emulator/Listener.java
+++ src/main/java/ru/mts/emulator/Listener.java
@@ -3,20 +3,43 @@ package ru.mts.emulator;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
+import javax.jms.*;
+
+import static ru.mts.emulator.Application.responseQueue;
 
 @Component
 public class Listener {
 
     @JmsListener(destination = Application.requestQueue)
-    public void receiveMessage(Message msg) throws JMSException {
+    public void receiveMessage(Message msg, Session session) throws JMSException {
+
+        String text;
+
+        if (msg instanceof TextMessage){
+            text = ((TextMessage) msg).getText();
+        }
+        else {
+            text = msg.toString();
+        }
+
 
         System.out.println();
         System.out.println("========================================");
-        System.out.println("Received message is: " + msg.getBody(String.class));
+
+        System.out.println("Responder received message: " + text);
+        System.out.println("           Redelivery flag: " + msg.getJMSReplyTo());
         System.out.println("========================================");
 
+
+        final String msgID = msg.getJMSMessageID();
+        final String corrID = msg.getJMSCorrelationID();
+
+
+        MessageProducer replyDest = session.createProducer(msg.getJMSDestination());
+        TextMessage replyMsg = session.createTextMessage("Replying to " + text);
+        replyMsg.setJMSCorrelationID(corrID);
+        replyMsg.setJMSMessageID(msgID);
+        replyDest.send(replyMsg);
 
 
     }
